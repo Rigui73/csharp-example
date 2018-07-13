a62870e34188097212a5bea6a57581642f187311
Adria Rigall <contacto@adriarigall.com>
2018-07-13 09:12:18 +0200

    Agregar las clases en carpeta Models/.

---
+++ csharp-example/csharp-example/Models/Administrador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_example.Models
+{
+    public class Administrador : Persona
+    {
+        public Administrador(string nombre, string apellido)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+    }
+}
---
+++ csharp-example/csharp-example/Models/Banco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_example.Models
+{
+    public class Banco
+    {
+        public string Nombre { get; set; }
+        public List<Cliente> ListaClientes { get; set; }
+        public Administrador Admin { get; set; }
+
+        public Banco(Administrador admin, string nombre)
+        {
+            this.Nombre = nombre;
+            this.Admin = admin;
+            ListaClientes = new List<Cliente>();
+        }
+    }
+}
